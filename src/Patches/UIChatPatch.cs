43c99708f87d1ef74c8e44ad6f4103537597ab1e
wenright <wenright0@gmail.com>
2025-06-21 20:37:54 -0500

    Add powerup check key

--- src/Patches/UIChatPatch.cs
+++ src/Patches/UIChatPatch.cs
@@ -11,23 +11,36 @@ public static class UIChatPatch
   [HarmonyPrefix]
   public static bool Patch_UIChat_Server_ProcessPlayerChatMessage(Player player, string message, ulong clientId, bool useTeamChat, bool isMuted, SerializedDictionary<int, string[]> ___quickChatMessages, UIChat __instance)
   {
-    if (!NetworkManager.Singleton.IsServer || message != ___quickChatMessages[0][0])
+    if (!NetworkManager.Singleton.IsServer || (message != ___quickChatMessages[0][0] && message != ___quickChatMessages[0][1]))
       return Constants.CONTINUE;
-
+    
     if (!PlayerBodyV2_Patch.powerupManagers.TryGetValue(player, out PowerupManager powerupManager)) return Constants.CONTINUE;
+
+    float msRemaining = powerupManager.nextPowerupAvailableAt - Time.time;
+    string formattedMsRemaining = msRemaining.ToString("0.00");
+
+    if (message == ___quickChatMessages[0][1])
+    {
+      if (powerupManager.CanUse())
+      {
+        __instance.Server_ChatMessageRpc($"<b><color={powerupManager.availablePowerup.color}>{powerupManager.availablePowerup.name}</color></b> is ready to use", UIChat.Instance.RpcTarget.Group(new[] { player.OwnerClientId }, RpcTargetUse.Temp));
+      }
+      else
+      {
+        __instance.Server_ChatMessageRpc($"Powerup on cooldown for <b>{formattedMsRemaining}</b>s", __instance.RpcTarget.Group(new[] { player.OwnerClientId }, RpcTargetUse.Temp));
+      }
+      return Constants.SKIP;
+    }
 
     if (!powerupManager.CanUse())
     {
-	    float msRemaining = powerupManager.nextPowerupAvailableAt - Time.time;
-		  string formattedMsRemaining = msRemaining.ToString("0.00");
-
       __instance.Server_ChatMessageRpc($"Powerup on cooldown for <b>{formattedMsRemaining}</b>s", __instance.RpcTarget.Group(new[] { player.OwnerClientId }, RpcTargetUse.Temp));
       return Constants.SKIP;
     }
 
-    Powerup PowerupUsed = powerupManager.UsePowerup();
+    Powerup powerupUsed = powerupManager.UsePowerup();
 
-    __instance.Server_ChatMessageRpc(__instance.WrapPlayerUsername(player) + $" used <b><color={PowerupUsed.color}>{PowerupUsed.name}</color></b>", __instance.RpcTarget.ClientsAndHost);
+    __instance.Server_ChatMessageRpc(__instance.WrapPlayerUsername(player) + $" used <b><color={powerupUsed.color}>{powerupUsed.name}</color></b>", __instance.RpcTarget.ClientsAndHost);
 
     return Constants.SKIP;
   }
