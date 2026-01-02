d9349aea66b2cdd7928dd2ff4314bf1347222b96
rnterbush <ryan.terbush@gmail.com>
2026-01-02 14:46:51 -0800

    Re-enable other powerups

--- src/Powerup.cs
+++ src/Powerup.cs
@@ -33,14 +33,14 @@ public static class PowerupList
 {
   public static Dictionary<string, Powerup> dict = new Dictionary<string, Powerup>()
   {
-    //[PowerupNames.Magnet] = new Powerup(PowerupNames.Magnet, 6.0f, "magenta"),
-    //[PowerupNames.Lasso] = new Powerup(PowerupNames.Lasso, 1.0f, "cyan"),
-    //[PowerupNames.Rage] = new Powerup(PowerupNames.Rage, 7.0f, "purple"),
-    //[PowerupNames.Grapple] = new Powerup(PowerupNames.Grapple, 7.0f, "teal"),
-    //[PowerupNames.Punch] = new Powerup(PowerupNames.Punch, 0.5f, "orange"),
-    //[PowerupNames.Glue] = new Powerup(PowerupNames.Glue, 7.5f, "yellow"),
-    //[PowerupNames.Kick] = new Powerup(PowerupNames.Kick, 0.5f, "lightblue"),
-    //[PowerupNames.LowGrav] = new Powerup(PowerupNames.LowGrav, 5.0f, "green"),
+    [PowerupNames.Magnet] = new Powerup(PowerupNames.Magnet, 6.0f, "magenta"),
+    [PowerupNames.Lasso] = new Powerup(PowerupNames.Lasso, 1.0f, "cyan"),
+    [PowerupNames.Rage] = new Powerup(PowerupNames.Rage, 7.0f, "purple"),
+    [PowerupNames.Grapple] = new Powerup(PowerupNames.Grapple, 7.0f, "teal"),
+    [PowerupNames.Punch] = new Powerup(PowerupNames.Punch, 0.5f, "orange"),
+    [PowerupNames.Glue] = new Powerup(PowerupNames.Glue, 7.5f, "yellow"),
+    [PowerupNames.Kick] = new Powerup(PowerupNames.Kick, 0.5f, "lightblue"),
+    [PowerupNames.LowGrav] = new Powerup(PowerupNames.LowGrav, 5.0f, "green"),
     [PowerupNames.Tornado] = new Powerup(PowerupNames.Tornado, 6.0f, "red"),
   };
 }
